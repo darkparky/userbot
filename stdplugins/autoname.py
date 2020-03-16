a68b3dce920e74dc2441d2b7d6d83d343019e2fb
viperadnan-dummy <60770759+viperadnan-dummy@users.noreply.github.com>
2020-03-16 20:25:36 +0530

    Update autoname.py

--- stdplugins/autoname.py
+++ stdplugins/autoname.py
@@ -6,7 +6,7 @@ import time
 from telethon.tl import functions
 from telethon.errors import FloodWaitError
 from uniborg.util import admin_cmd
-from userbot import AUTONAME
+from uniborg import AUTONAME
 
 
 DEL_TIME_OUT = 60
