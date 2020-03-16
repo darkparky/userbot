1224f2d336655f73a07fff424685cd053b9b1ba6
viperadnan-dummy <60770759+viperadnan-dummy@users.noreply.github.com>
2020-03-16 20:39:32 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -95,6 +95,9 @@ class Config(object):
     GOOGLE_CHROME_DRIVER = os.environ.get("GOOGLE_CHROME_DRIVER", None)
     GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
     #
+    CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
+    CHROME_BIN = os.environ.get("CHROME_BIN", None)
+    #
     LYDIA_API = os.environ.get("LYDIA_API", None)
     #
     # define "spam" in PMs
